4f3adb313e1dd053fc589a35a094fb7c17b6919e
Maryem Badaoui <maryem.badaoui.0608@gmail.com>
2024-03-27 23:05:22 +0000

    Creation des class

---
+++ PFE_CHU/Models/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace PFE_CHU.Models
+{
+    public class Role
+    {
+        int? id { get; set; }
+        string libelle { get; set; }
+
+        ICollection<user >   users{ get; set; }
+    }
+}
---
+++ PFE_CHU/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Eventing.Reader;
+
+namespace PFE_CHU.Models
+{
+    public class user
+    {
+        int? id { get; set; }
+        string nom { get; set; }
+        string prenom { get; set; }
+        string login { get; set; }
+        [DataType(DataType.Password)]
+        string password { get; set; }
+        [NotMapped]
+        [DataType(DataType.Password)]
+        string confirmepswd { get; set; }
+        Role role { get; set; }
+        int RoleId { get; set; }
+ 
+
+
+    }
+}
